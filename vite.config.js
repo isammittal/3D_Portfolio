8a95f9e6dd79710a71c68c5523d8086b0f8e219f
isammittal <sammittal2010@gmail.com>
2025-05-19 17:34:47 +0530

    Quick update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
